a013e1c080904f83e648c64653f2421c72a0f216
BohdanMoroz <bohdanmoroz@gmail.com>
2017-09-28 15:10:28 +0300

    Done project

--- js/line.js
+++ js/line.js
@@ -1,3 +1,28 @@
-var el = document.getElementById("rightline")
-el.style.width = "100%"
-alert("done")
+function show() {
+	document.getElementById('nav').style.display = 'block';
+	document.getElementById('header').style.height = 160 + "px";
+	document.getElementById('page-title').style.marginTop = 60 + "px";
+	document.getElementById('burger').style.backgroundImage = "url(../../img/cross.png)";
+	// alert("show")
+}
+
+function hide() {
+	document.getElementById('nav').style.display = 'none';
+	document.getElementById('header').style.height = 50 + "px";
+	document.getElementById('page-title').style.marginTop = -30 + "px";
+	document.getElementById('burger').style.backgroundImage = "url(../../img/burger.png)";
+	// alert("hide")
+}
+
+var isPushed = false;
+
+function menu() {
+	// alert(isPushed);
+	if (!isPushed) {
+		show()
+		isPushed = true;
+	} else {
+		hide()
+		isPushed = false;
+	}
+}
